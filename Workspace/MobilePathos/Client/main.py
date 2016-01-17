6ad58677ed5619aeea668f1b37eb0e1e939c0aa2
MeDzIeDaRbI <medzied.arbi@supcom.tn>
2016-01-17 19:15:02 +0100

    Update main.py

--- Workspace/MobilePathos/Client/main.py
+++ Workspace/MobilePathos/Client/main.py
@@ -1,1 +1,24 @@
 
+'''
+  DOCUMENTATION
+'''
+
+# IMPORTS
+import entry as en
+import recording as rec
+import previous as prev
+import processing as proc
+
+# ENTRY POINT
+en.animate() ;
+
+# START RECORDING
+rec.record()
+
+# IMPORT PREVIOUS CALCULATIONS DATA
+prev.importPrevious()
+
+# BEGIN PROCESSING
+proc.processing()
+
+# DISPLAY RESULTS
